3fc6bcea5ff9d69ac98ae0d8feaf6a6b06c14718
Deepak9103 <deepusingh9104gmail.com>
2026-01-13 11:08:41 +0530

    string method

---
+++ function.js
@@ -0,0 +1,25 @@
+
+console.log("Hello".length);
+//5
+console.log("Hello".charAt(2));
+//l
+console.log("Hello".indexOf("o"));
+//4
+console.log("Hello".toUpperCase());
+//HELLO
+console.log("Hello".toLowerCase());
+//hello
+console.log("    Hello   ".trim());
+//Hello
+console.log("Hello Deepak".replace("Deepak","Deep"));
+//Hello Deep
+console.log("Hello".slice(1,4));
+//ell
+console.log("Hello".split());
+//
+console.log("Hello".startsWith("He"));
+//true
+console.log("Hello".endsWith("o"));
+//true
+console.log("Hello".includes("ll"));
+//true
